6f842c5a9bb1079876592ee34bca838eae4c58c2
qualisign <djoseph@optipulse.com>
2017-01-11 23:06:37 +0100

    initial commit

---
+++ homepage/js/index.js
@@ -0,0 +1,33 @@
+var tags = {
+    "english": ["semiotics",
+                "philosophy",
+                "programming",
+                "blockchain",
+                "daoism",
+                "buddhism",
+                "chinese medicine",
+                "qigong",
+                "fascism",
+                "drugs",
+                "formal logic",
+                "smart contracts",
+                "marxism",
+                "anarchism",
+                "linguistics",
+                "mnemonics",
+                "economics",                
+                "cryptocurrencies",
+                "fiction",
+                "politics",                
+                "media",                
+                "history",
+                "anthropology",
+                "religion",
+                "mythology",
+                "mathematics",
+                
+               
+]}
+
+$("#tags").text(tags["english"].join(", "));
+console.log(tags["english"].join(", "));
